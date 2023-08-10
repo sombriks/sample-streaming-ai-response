e8e231390b5ded9cd6f7361336fd58f47c3df19c
Leonardo Silveira <sombriks@gmail.com>
2023-08-10 17:53:58 -0300

    wiring sample projetc skeleton

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sample-streaming-ai-response
+
 investigating proper openai chat streaming responses over Server-Sent Events
+
---
+++ ai-demo/src/main/kotlin/com/example/aidemo/AiDemoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.aidemo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AiDemoApplication
+
+fun main(args: Array<String>) {
+	runApplication<AiDemoApplication>(*args)
+}
